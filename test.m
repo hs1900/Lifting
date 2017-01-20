c0ff7f0e34f355cc8433a73ac746f9a375146eaa
hs1900 <hs1900@nyu.edu>
2017-01-20 14:20:32 -0500

    Update test.m
    just using frame difference

--- test.m
+++ test.m
@@ -7,3 +7,14 @@ mov(1:nFrames)= struct('cdata', zeros(vidHeight, vidWidth, 3, 'uint8'))
 for i = 1 : nFrames
   mov(i).cdata = read(vidObj, k); 
 end
+
+% Taking the difference between adjacent frames
+% Assuming the foreground is the person of interest
+frame1(1) = mov(1);
+for i = 2 : nFrame
+ diff = abs(frame1(i-1) – mov(i));
+ if diff >=64
+  th = 1;
+  else th = 0;
+ frame1(i) = mov(i);
+end
