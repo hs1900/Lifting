be0a7e33e5fdafe8c7fc61a1d06879806364a3dc
thisiseric95 <ew1131@nyu.edu>
2017-01-27 16:27:32 -0500

    Update test.m

--- test.m
+++ test.m
@@ -2,17 +2,11 @@ clear;clc;
 
 Obj = VideoWriter('60frames.avi');
 writerObj.FrameRate = 60;
-open(Obj);
 
 vidObj = VideoReader('IMG_0410.mov'); 
 nFrames = vidObj.NumberOfFrames;
 vidHeight = vidObj.Height; 
 vidWidth = vidObj.Width;
-
-% mov(1:nFrames)= struct('cdata', zeros(vidHeight, vidWidth, 3, 'uint8'), 'colormap', []);
-% for i = 1 : nFrames
-%   mov(i).cdata = read(vidObj, i); 
-% end
 
 % Taking the difference between adjacent frames
 % Assuming the foreground is the person of interest
@@ -32,6 +26,7 @@ end
 %downsampling
 downDiffT = diffT(1:2:end,1:2:end,:);
 
+open(Obj);
 for i = 2: 100
     writeVideo(Obj,double((diffT(:,:,i-1))));
 end    
