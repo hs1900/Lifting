23b25d7af039e7712511f4094ad2641b8f50a7e3
Henry Shao <hs1900@nyu.edu>
2017-03-23 23:59:25 -0400

    update with boundaries and butt matching

--- GMM.m
+++ GMM.m
@@ -1,7 +1,7 @@
 %% GMM
 clear;clc;close all;
 
-Obj = VideoWriter('GMM.avi');
+Obj = VideoWriter('GMMbox.avi');
 writerObj.FrameRate = 30;
 open(Obj);
 
@@ -9,24 +9,79 @@ vidObj = VideoReader('badformshade.m4v');
 nFrames = vidObj.NumberOfFrames;
 vidHeight = vidObj.Height; 
 vidWidth = vidObj.Width;
-%FGvid = zeros(vidHeight,vidWidth,100);
-% Start with first 10 frames
-foregroundDetector = vision.ForegroundDetector('NumGaussians', 3,'NumTrainingFrames', 20,'LearningRate',.0001);
-% Finish with the first 600 frames
+
+% Train frames with no foreground
+foregroundDetector = vision.ForegroundDetector('NumGaussians', 3,'NumTrainingFrames', 130,'LearningRate',.0001);
+% Complete background subtraction
 videoReader = vision.VideoFileReader('badformshade.m4v');
-for i = 1:700
+seOpen = strel('square', 12);
+%  seClose = strel('square', 1);
+BBox = zeros(nFrames,4);
+for i = 1:nFrames
     frame = step(videoReader); % read the next video frame
-    foreground = step(foregroundDetector, frame);
-    
-    writeVideo(Obj,double(foreground));
-    
-%     se = strel('square', 3);
-%     filteredForeground = imopen(foreground, se);
-%     writeVideo(Obj,double(filteredForeground));
+    foreground = step(foregroundDetector, frame);    
+%   writeVideo(Obj,double(foreground));    
+    IMopen = imopen(foreground,seOpen);
+%   IMclose = imclose(foreground,seClose);   
+
+% blob analysis
+blobAnalysis = vision.BlobAnalysis('BoundingBoxOutputPort', true, ...
+    'AreaOutputPort', false, 'CentroidOutputPort', false, ...
+    'MinimumBlobArea', 4000);
+bbox = step(blobAnalysis, IMopen);
+
+index = find(max(bbox(:,3) .*bbox(:,4)));
+if size(bbox) > [0 3]
+BBox(i,:) = bbox(index,:);
 end
+
+result = insertShape( 255*uint8(IMopen), 'Rectangle', BBox(i,:), 'Color', 'green');
+
+IMopen = result;
+
+    writeVideo(Obj,(IMopen));
+end
+
 close(Obj);
-figure; imshow(frame); title('Video Frame');
-figure; imshow(foreground); title('Foreground');
+
+%% Matching butt
+cutoff= floor(nFrames*(1/3));
+tempbox = BBox(cutoff:2*cutoff,3);
+tempmax = find(tempbox == max(tempbox));
+endFrameLoc = tempmax+cutoff;
+vidObj2 = VideoReader('GMMbox.avi'); 
+
+EndFrame = read(Obj2,endFrameLoc);
+imshow(uint8(EndFrame));
+%% Edge
+edgeObj = VideoWriter('edgeGMM.avi');
+GMMObj = VideoReader('GMM.avi'); 
+open(edgeObj);
+for i = 1:nFrames
+    frame = rgb2gray(read(GMMObj,i)); % read the next video frame
+    edgeFrame = edge(frame, 'canny',.5,2);
+    writeVideo(edgeObj,double(edgeFrame));
+end
+close(edgeObj);
+edgeObj = VideoReader('edgeGMM.avi');
+testFrame = rgb2gray(read(edgeObj,780));
+imshow(testFrame);
+%% testing
+clc;
+GMMObj = VideoReader('GMM.avi'); 
+
+testFrame = rgb2gray(read(GMMObj,780));
+bwFrame = bwboundaries(testFrame);
+
+BW = testFrame > 128;
+[B,L] = bwboundaries(BW,'noholes');
+figure;
+imshow(label2rgb(L, @jet, [.5 .5 .5]))
+hold on;
+for k = 1:length(B)
+   boundary = B{k};
+   plot(boundary(:,2), boundary(:,1), 'w', 'LineWidth', 2)
+end
 %% Morphological
 se = strel('square', 3);
 filteredForeground = imopen(foreground, se);
@@ -44,9 +99,9 @@ result = insertShape(frame, 'Rectangle', bbox, 'Color', 'green');
 numCars = size(bbox, 1);
 result = insertText(result, [10 10], numCars, 'BoxOpacity', 1, ...
     'FontSize', 14);
-figure; imshow(result); title('Detected Cars');
+figure; imshow(result); title('Detected');
 %% Process
-videoPlayer = vision.VideoPlayer('Name', 'Detected Cars');
+videoPlayer = vision.VideoPlayer('Name', 'Detected');
 videoPlayer.Position(3:4) = [650,400];  % window size: [width, height]
 se = strel('square', 3); % morphological filter for noise removal
 
