fb984a944b5d2696112a329a370cae9b5e465e59
Henry Shao <hs1900@nyu.edu>
2017-04-06 23:02:29 -0400

    Update GMM.m

--- GMM.m
+++ GMM.m
@@ -1,21 +1,26 @@
 %% GMM
 clear;clc;close all;
-
-Obj = VideoWriter('GMMbox.avi');
+GMM = 'GMM.avi';
+Obj = VideoWriter(GMM);
 writerObj.FrameRate = 30;
 open(Obj);
 
-vidObj = VideoReader('badformshade.m4v'); 
+vidIn = 'badformshade.m4v';
+vidObj = VideoReader(vidIn); 
 nFrames = vidObj.NumberOfFrames;
 vidHeight = vidObj.Height; 
 vidWidth = vidObj.Width;
 
-% Train frames with no foreground
+% Train frames 
 foregroundDetector = vision.ForegroundDetector('NumGaussians', 3,'NumTrainingFrames', 130,'LearningRate',.0001);
-% Complete background subtraction
-videoReader = vision.VideoFileReader('badformshade.m4v');
+
+% Complete background subtraction and Write Video
+videoReader = vision.VideoFileReader(vidIn);
+
+% Morphological parameter
 seOpen = strel('square', 12);
 %  seClose = strel('square', 1);
+
 BBox = zeros(nFrames,4);
 for i = 1:nFrames
     frame = step(videoReader); % read the next video frame
@@ -24,7 +29,7 @@ for i = 1:nFrames
     IMopen = imopen(foreground,seOpen);
 %   IMclose = imclose(foreground,seClose);   
 
-% blob analysis
+% Blob Bnalysis to Obtain Boundary
     blobAnalysis = vision.BlobAnalysis('BoundingBoxOutputPort', true, ...
         'AreaOutputPort', false, 'CentroidOutputPort', false, ...
         'MinimumBlobArea', 4000);
@@ -34,130 +39,70 @@ for i = 1:nFrames
     if size(bbox) > [0 3]
         BBox(i,:) = bbox(index,:);
     end
+    
+% Can Insert Box for Visual Confirmation
+%     result = insertShape( 255*uint8(IMopen), 'Rectangle', BBox(i,:), 'Color', 'green');
+%     IMopen = result;
 
-    result = insertShape( 255*uint8(IMopen), 'Rectangle', BBox(i,:), 'Color', 'green');
-    IMopen = result;
-    writeVideo(Obj,(IMopen));
+    writeVideo(Obj,255*uint8(IMopen));
 end
 
 close(Obj);
 
-%% Matching butt
+%% Matching Lower Back
+
+% Set Range and Analyze
 cutoff= floor(nFrames*(1/3));
 tempbox = BBox(cutoff:2*cutoff,3);
+
+% Locate Butt
 tempmax = find(tempbox == max(tempbox));
 endFrameLoc = tempmax+cutoff-1;
-vidObj2 = VideoReader('GMM.avi'); 
+vidObj2 = VideoReader(GMM); 
 
 EndFrame = (read(vidObj2,endFrameLoc));
-imshow(uint8(EndFrame));
+% imshow(uint8(EndFrame));
 
 EndFrameBBox = BBox(endFrameLoc,:);
 buttCol = EndFrameBBox(1)+EndFrameBBox(3)-1;
 buttRow = find(EndFrame(:,buttCol) > 50,1);
 
-% Create template around butt
+% Create Template Around Butt
 buttFrame = rgb2gray(read(vidObj,endFrameLoc));
-% buttTemp = buttFrame(buttRow-30:buttRow+30,buttCol-60:buttCol);
 
-bibuttTemp = EndFrame(buttRow-25:buttRow+20,buttCol-40:buttCol,1);
+backTempColStart = round(EndFrameBBox(3)*(1/3)); %first hardcoded the size of the box for our image
+backTempColEnd = round(EndFrameBBox(3)*(1/15));
+backTempRowStart = round(EndFrameBBox(4)*(4/5));
 
-% Create new template closer to the lower back
-buttCol=buttCol-40;
-buttRow = buttRow -25 + find(bibuttTemp(:,1) > 50,1);
-backTemp = buttFrame(buttRow-40:buttRow,buttCol-10:buttCol+20);
-% tempxy = zeros(nFrames,2); tempxy(endFrameLoc,:) = [buttRow-8,buttCol-8] ;
-% offset tempxy by 8
- 
-for m = endFrameLoc-1:-1:100
+% buttRow-80, buttRow                         buttCol-50:buttCol+10
+% Create New Template for Back
+backTemp = buttFrame(buttRow-backTempRowStart:buttRow, ...
+           buttCol-backTempColStart:buttCol+backTempColEnd);
+
+ %Template Match Lower Back
+for m = endFrameLoc-1:-1:cutoff
     prevFrame = rgb2gray(read(vidObj,m));
-%     prevBox = prevFrame(BBox(m,2):BBox(m,2)+BBox(m,4),BBox(m,1):BBox(m,1)+BBox(m,3));
-%     [tempRow,tempCol] = size(buttTemp);
-    [dx, dy, matchblock] = templatematching(backTemp,prevFrame,buttCol-10,buttRow-40 ,1);
-%     check to see if matchblock is correct
-     if mod(m,2)==0     
-         figure;imshow(uint8(matchblock));
-     end
+    [dx, dy, matchblock] = templatematching(backTemp,prevFrame,buttCol-backTempColStart,buttRow-backTempRowStart ,3);
+
+    imshow(uint8(matchblock));
     backTemp = matchblock;
-%     tempxy( -m+endFrameLoc,:) = [dx +tempxy(-m+endFrameLoc-1,1), dy+tempxy(-m+endFrameLoc-1,2)];
+    
     buttCol = buttCol+dx;
     buttRow = buttRow+dy;
+    
+    buttCoords(m,1) = buttCol-backTempColStart;
+    buttCoords(m,2) = buttRow-backTempRowStart;
 end
 
-%% Butt Match Foward
-cutoff= floor(nFrames*(1/3));
-tempbox = BBox(cutoff:2*cutoff,3);
-tempmax = find(tempbox == max(tempbox));
-endFrameLoc = tempmax+cutoff-1;
-vidObj2 = VideoReader('GMM.avi'); 
+%% Matching Lower Back Foward
 
-EndFrame = (read(vidObj2,endFrameLoc));
-imshow(uint8(EndFrame));
 
-EndFrameBBox = BBox(endFrameLoc,:);
-buttCol = EndFrameBBox(1)+EndFrameBBox(3)-1;
-buttRow = find(EndFrame(:,buttCol) > 50,1);
-
-% Create template around butt
-buttFrame = rgb2gray(read(vidObj2,endFrameLoc));
-buttTemp = buttFrame(buttRow-25:buttRow+20,buttCol-40:buttCol);
-
-% Create new template closer to the lower back
-% buttCol=buttCol-40;
-% buttRow = buttRow -25 + find(bibuttTemp(:,1) > 50,1);
-% backTemp = buttFrame(buttRow-40:buttRow,buttCol-10:buttCol+20);
- 
-for m = endFrameLoc+1:1:850
-    nextFrame = rgb2gray(read(vidObj,m));
-    [dx, dy, matchblock] = templatematching(buttTemp,nextFrame,buttCol-40,buttRow-25 ,1);
-%     check to see if matchblock is correct 
-  if mod(m,2)==0     
-         figure;imshow(uint8(matchblock));
-  end      
-    buttTemp = matchblock;
-%     tempxy( -m+endFrameLoc,:) = [dx +tempxy(-m+endFrameLoc-1,1), dy+tempxy(-m+endFrameLoc-1,2)];
-    buttCol = buttCol+dx;
-    buttRow = buttRow+dy;
-end
-%% Testing EBMA
-m = 25;
-prevFrame = rgb2gray(read(vidObj,m));
-template = buttTemp; img = prevframe; x0 = buttCol-20; y0 = buttRow - 50; R = 2;
-
-[H,W]=size(img);
-[BH,BW]=size(template);
-maxerror=BH*BW*255;
-for (k=max(1,x0-R):min(W-BW,x0+R))
-    for (l=max(1,y0-R):min(H-BH,y0+R))
-        block=img(l:l+BH-1,k:k+BW-1);
-        error=sum(sum(abs(block-template)));
-       if (error<maxerror)
-             dx=k-x0;dy=l-y0;matchblock=block;
-             maxerror=error;
-       end
-    end
-end
-%% HOG 
-peopleDetector = vision.PeopleDetector;
-y = step(peopleDetector,endFrameLoc) ;
-figure;imshow(y);
-%% Edge
-edgeObj = VideoWriter('edgeGMM.avi');
-GMMObj = VideoReader('GMM.avi'); 
-open(edgeObj);
-for i = 1:nFrames
-    frame = rgb2gray(read(GMMObj,i)); % read the next video frame
-    edgeFrame = edge(frame, 'canny',.5,2);
-    writeVideo(edgeObj,double(edgeFrame));
-end
-close(edgeObj);
-edgeObj = VideoReader('edgeGMM.avi');
-testFrame = rgb2gray(read(edgeObj,780));
-imshow(testFrame);
 %% Testing Curvatures
-clc;
-GMMObj = VideoReader('GMM.avi'); 
+clc;close all;
+GMMObj = VideoReader(GMM); 
+fontSize = 20;
 
+% Use Face to Obtain Frame 
 testFrame = rgb2gray(read(GMMObj,780));
 bwFrame = bwboundaries(testFrame);
 
@@ -169,7 +114,7 @@ hold on;
 for k = 1:length(B)
    boundary = B{k};
    plot(boundary(1:floor(length(boundary)/30),2), ...
-   boundary(1:floor(length(boundary)/30),1), 'b', 'LineWidth', 2)
+   boundary(1:floor(length(boundary)/30),1), 'r', 'LineWidth', 2)
 end
 
 %find radius and curvature
@@ -196,19 +141,39 @@ for t = 1 : numberOfPoints
 	y2 = boundary(index2,1);
 	x3 = boundary(index3,2);
 	y3 = boundary(index3,1);
-	% Now call Roger's formula:
-	% http://www.mathworks.com/matlabcentral/answers/57194#answer_69185
+
 	curvature(t) = 2*abs((x2-x1).*(y3-y1)-(x3-x1).*(y2-y1)) ./ ...
 	sqrt(((x2-x1).^2+(y2-y1).^2)*((x3-x1).^2+(y3-y1).^2)*((x3-x2).^2+(y3-y2).^2));
 end
 
 % Plot curvature.
-figure;
-plot(curvature, 'b-', 'LineWidth', 1)
+figure;plot(curvature, 'b-', 'LineWidth', 1)
 grid on;
 xlim([1 numberOfPoints]); % Set limits for the x axis.
 title('Radius of Curvature', 'FontSize', fontSize);
 
+threshold = 1;
+cur = curvature > threshold;
+Candidates = cur .*curvature;
+figure;plot(Candidates);
+%% HOG 
+peopleDetector = vision.PeopleDetector;
+y = step(peopleDetector,endFrameLoc) ;
+figure;imshow(y);
+%% Edge
+edgeObj = VideoWriter('edgeGMM.avi');
+GMMObj = VideoReader('GMM.avi'); 
+open(edgeObj);
+for i = 1:nFrames
+    frame = rgb2gray(read(GMMObj,i)); % read the next video frame
+    edgeFrame = edge(frame, 'canny',.5,2);
+    writeVideo(edgeObj,double(edgeFrame));
+end
+close(edgeObj);
+edgeObj = VideoReader('edgeGMM.avi');
+testFrame = rgb2gray(read(edgeObj,780));
+imshow(testFrame);
+%% References Below
 %% Morphological
 se = strel('square', 3);
 filteredForeground = imopen(foreground, se);
